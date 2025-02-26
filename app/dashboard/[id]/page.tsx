3c54c09a9372520b14692c93bbedb6f80f5e4a95
Fransedo Ravelino <fransedo.ravelino@bluebirdgroup.com>
2025-02-26 10:28:45 +0700

    fix build error

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -3,19 +3,21 @@
 import DocumentForm from "../new/components/DocumentForm";
 import DocumentHeader from "../new/components/DocumentHeader";
 import WYSIWYG from "@/app/components/WYSIWYG";
-import DocumentSimilarity from "../new/components/DocumentSimilarity";
 import { useNewsDetail } from "../queries/news";
 import AiTimesLoader from "@/app/components/AiTimesLoader";
 import { useState } from "react";
+import { useParams } from "next/navigation";
+import DocumentSidebar from "../new/components/Sidebar/DocumentSidebar";
 
-export default function Page({ params }: { params: { id: string } }) {
+export default function Page() {
+  const params = useParams<{ id: string }>();
   const { data } = useNewsDetail(params.id);
   const [isEditingNews, setIsEditingNews] = useState(false);
   return (
     <>
       {data ? (
         <div className="flex">
-          <div className="w-[350px] border-r border-zinc-300 h-screen overflow-auto shrink-0">
+          <div className="w-[300px] border-r border-zinc-300 h-screen overflow-auto shrink-0">
             <DocumentForm defaultValues={data} />
           </div>
           <div className="flex flex-col gap-5 p-5 w-full">
@@ -23,7 +25,7 @@ export default function Page({ params }: { params: { id: string } }) {
             <WYSIWYG initialContent={data.rawContent} readonly={!isEditingNews} />
           </div>
           <div className="shrink-0">
-            <DocumentSimilarity />
+            <DocumentSidebar />
           </div>
         </div>
       ) : (
