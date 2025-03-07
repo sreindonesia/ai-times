88f9d3c734f2a6bf6382c843759d4c9da64d6020
Fransedo Ravelino <fransedo.ravelino@bluebirdgroup.com>
2025-03-07 11:09:18 +0700

    added back to home button in detail news page

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -8,6 +8,8 @@ import AiTimesLoader from "@/app/components/AiTimesLoader";
 import { useState } from "react";
 import { useParams } from "next/navigation";
 import DocumentSidebar from "../new/components/Sidebar/DocumentSidebar";
+import Link from "next/link";
+import { ArrowLeft } from "flowbite-react-icons/outline";
 
 export default function Page() {
   const params = useParams<{ id: string }>();
@@ -17,8 +19,12 @@ export default function Page() {
     <>
       {data ? (
         <div className="flex">
-          <div className="w-[300px] border-r border-zinc-300 h-screen overflow-auto shrink-0">
-            <DocumentForm onSubmit={() => console.log("hello")} />
+          <div className="w-[300px] border-r border-zinc-300 h-screen overflow-auto pr-[30px]  py-5 pl-5 shrink-0">
+            <Link href={"/dashboard"} className="flex items-center gap-2.5 mb-5">
+              <ArrowLeft size={32} />
+              Back to home
+            </Link>
+            <DocumentForm onSubmit={() => console.log("docs")} />
           </div>
           <div className="flex flex-col gap-5 p-5 w-full">
             <DocumentHeader onEdit={() => setIsEditingNews((prev) => !prev)} />
